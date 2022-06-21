ea22ce816274169adfc02fcd6cb6800a8ea07778
RATAN1118 <ratanvk4@gmail.com>
2022-06-21 14:15:10 +0530

    4th

--- test_client.py
+++ test_client.py
@@ -1,17 +1,27 @@
 import paramiko
 import re
 
+import pytest
+
 output_file = "paramiko.log"
+@pytest.fixture
+def connection():
+    port = 22
+    global client
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname="192.168.170.104", port=22, username="node1", password="Rk@8531")
+    yield
+    client.close()
 
 
-def test_operation():
+
+
+def test_operation(connection):
     print("paramiko opration")
     try:
-        port = 22
-        client = paramiko.SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(hostname="192.168.43.166", port=22, username="node1", password="Rk@8531")
+
         (stdin, stdout, stderr) = client.exec_command("df --output=source,pcent /dev/sda3")
         (stdin1, stdout1, stderr1) = client.exec_command("date")
         cmd_output = stdout.read()
@@ -28,4 +38,5 @@ def test_operation():
 
         return output_file
     finally:
-        client.close()
+        print("Operations Successful")
+
